adc1726446e7fd2da392f7009e75e5c8a482c7c1
hohserg1 <35610168+hohserg1@users.noreply.github.com>
2020-11-08 14:20:43 +0300

    Added feature of github token destruction

--- player-monitoring/home/lib/hoverhelm/addition_radar.lua
+++ player-monitoring/home/lib/hoverhelm/addition_radar.lua
@@ -1,5 +1,6 @@
 local gistId = "" -- create new secret gist and insert id here
 local githubToken = "" -- visit https://github.com/settings/tokens for create and copy OAuth token for Gist
+local programLocation = "/home/lib/hoverhelm/addition_radar.lua" -- it file vill be removed when otusiderplayer will closer to main device
 local internet = require("component").internet
 
 return {
@@ -11,6 +12,9 @@ return {
                 {Authorization="token "..githubToken, Accept="application/vnd.github.v3+json"},
                 "POST"
             )
+        end,
+        hh_destroy_github_token = function(card, sender)
+            filesystem.remove(programLocation)
         end
     }
 }
