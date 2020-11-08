a1b0306ffc248bf8c68f232caf229f943c0d3d2e
hohserg1 <35610168+hohserg1@users.noreply.github.com>
2020-11-08 14:14:04 +0300

    Move gist upload feature to HoverHelm server addition

--- player-monitoring/radar.lua
+++ player-monitoring/radar.lua
@@ -2,21 +2,17 @@
 -- Required HoverHelm platform 
 -- https://computercraft.ru/topic/3771-hoverhelm-operatsionnaya-sistema-dlya-dronov-mikrokontrollerov-i-drugih-ustroystv-bez-sobstvennogo-zhestkogo-diska/
 
---WARNING! This program must be placed to device folder instead of core folder! Need for self-destroy `githubToken`
-
 --configuration
 local sleepTime = 1 --seconds scanning interval
-local gistId = "" -- create new secret gist and insert id here
-local githubToken = "" -- visit https://github.com/settings/tokens for create and copy OAuth token for Gist
 local programLocation = "/programs/radar.lua" -- it file vill be removed when otusiderplayer will closer to main device
-local screenWidth,screenHeight = 100,50
 local playersWhitelist = {["^^Cheburek^^"]=true}
-local radarModemsWhitelist = {}
 
 local detectors = map(component.list("os_entdetector"), component.proxy)
 local radars = map(component.list("radar"), component.proxy)
 local filesystem = component.filesystem
 local internet = component.internet
+
+local maxPacketSize = component.modem.maxPacketSize() - 4
 
 
 local scanFunctions = {}
@@ -49,12 +45,15 @@ end
 local gistSendingCache = {}
 
 local function gistFlush()
-    internet.request(
-        "https://api.github.com/gists/"..gistId.."/comments", 
-        '{"body":"'..table.concat(gistSendingCache,"\\n")..'"}', 
-        {Authorization="token "..githubToken, Accept="application/vnd.github.v3+json"},
-        "POST"
-    )
+    local content = table.concat(gistSendingCache,"\\n")
+    if #content > maxPacketSize then
+        for i=1, #content, maxPacketSize do
+            bios.card.send("hh_gist_log", content:sub(i, i+maxPacketSize-1))
+        end
+    else
+        bios.card.send("hh_gist_log", content)
+    end
+    
     gistSendingCache = {}
 end
 
@@ -116,5 +115,8 @@ while true do
     
     sleep(sleepTime)
 end
+    
+    
+
     
     
