0ac1f43cf56b19414f5ac75d8c5dc2a50b6108be
hohserg1 <35610168+hohserg1@users.noreply.github.com>
2020-11-12 13:04:10 +0300

    Create online-monitor-config.lua

---
+++ online-monitor/online-monitor-config.lua
@@ -0,0 +1,10 @@
+return {
+    {"Owner",{'XRASER','BeTePb',"hohserg"},0x8B1A1A},
+    {"Admin",{'SergeyZez','SplittingReality'},0xCC0000},
+    {"builder",{'AnshaABDUL372','MaxonZ',},0x473C8B},
+    {"Teh-Admin",{'lLuffy'},0xCC0000},
+    {"TrueMod",{'REAKTOR','iIiIiIiMuSiC_oF'},0x0066FF},
+    {"Modn",{'Sierr67','PIXOH','HolltiSS','BuTaMuH4iK','Bazoor','NightmareMoon'},0xCD2990},
+    {"Don-Mod",{'dark321','GangaMstar','Gra4_',},0xCC0000},
+    {"Helper",{'Night_Raven','TheRedMan','Duno'},0x00FF00}
+}
