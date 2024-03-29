ee833074c3f00da4c15eaa044b388b755c021548
hohserg1 <hohserg1@yandex.ru>
2024-03-30 00:02:49 +0300

    Create install.lua

---
+++ aBrickOS/install.lua
@@ -0,0 +1,2 @@
+os.execute("wget https://raw.githubusercontent.com/hohserg1/OpenComputersPrograms/master/aBrickOS/boot_compressed.lua /tmp/aBrickOS.lua")
+os.execute("flash /tmp/aBrickOS.lua aBrickOS")
